869c49f672fa3a7d61f5b888930cd9dfb9b95b12
warmansuganda <warman.suganda@delosaqua.com>
2023-05-07 22:03:53 +0700

    init http

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,33 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void getData() async {
+    var url = Uri.https('api.nytimes.com', 'svc/search/v2/articlesearch.json', {
+      'api-key': 'F81ff4Bj1S6cKDVQCC2XEWXMRaGpzaBA',
+      'page': '1',
+    });
+    Response response = await get(url);
+    if (response.statusCode == 200) {
+      Map jsonResponse = jsonDecode(response.body);
+      print(jsonResponse['copyright']);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
