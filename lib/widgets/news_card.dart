79843780e19f28667c1abe897b630843cc263fbc
warmansuganda <warman.suganda@gmail.com>
2023-05-24 17:46:00 +0700

    custom button style

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -9,11 +9,18 @@ class NewsCard extends StatelessWidget {
 
   final News item;
 
+  void handleBuy() {
+    // Function to handle the button press
+    print('Buy Now pressed!');
+    // Add your custom logic here
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.only(left: 10.0, right: 10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ClipRRect(
                 borderRadius: BorderRadius.circular(16.0),
@@ -30,7 +37,50 @@ class NewsCard extends StatelessWidget {
               style:
                   const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
             ),
-            const SizedBox(height: 3.0)
+            const SizedBox(height: 3.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Wrap(
+                  spacing: 8.0,
+                  children: [
+                    Text(
+                      'Category',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    Text(
+                      '•',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    Text(
+                      '5h',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+                OutlinedButton(
+                  onPressed: handleBuy,
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0))),
+                  child: const Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    spacing: 6.0,
+                    children: [
+                      Text(
+                        '\$ 50.000',
+                      ),
+                      Text(
+                        '|',
+                      ),
+                      Text(
+                        'Buy Now',
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
           ],
         ));
   }
