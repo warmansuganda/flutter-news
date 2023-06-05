90cdd14320e835b69c8579112183f6ad50840bc4
warmansuganda <warman.suganda@gmail.com>
2023-06-05 19:15:59 +0700

    add animation splashscreen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_news/screens/main_screen.dart';
+import 'package:flutter_news/widgets/logo.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/splash_screen';
@@ -11,38 +12,87 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  void _startMainScreen() {
+    Navigator.of(context).pushReplacement(
+      PageRouteBuilder(
+        transitionDuration: const Duration(milliseconds: 500),
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return FadeTransition(
+            opacity: animation,
+            child: const MainScreen(),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
 
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeOut,
+      ),
+    );
+
+    _animationController.repeat(reverse: true);
+
     Timer(const Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacementNamed(MainScreen.routeName);
+      // Navigator.of(context).pushReplacementNamed(MainScreen.routeName);
+      _startMainScreen();
     });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.blue,
-        child: Center(
-            child: Column(
+        width: double.infinity,
+        color: Colors.white,
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Flutter News',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 34,
-                  fontWeight: FontWeight.w700),
+          children: [
+            Expanded(
+              child: AnimatedBuilder(
+                animation: _animation,
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: 1.0 + (_animation.value * 0.2),
+                    child: child,
+                  );
+                },
+                child: const Logo(
+                  width: 200.0,
+                ),
+              ),
             ),
-            Text(
-              'Made with ❤ by Warman Suganda',
-              style: TextStyle(color: Colors.white, fontSize: 12),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Made with ❤ by Warman Suganda',
+                style: TextStyle(color: Colors.blue, fontSize: 12),
+              ),
             ),
           ],
-        )),
+        ),
       ),
     );
   }
--- lib/widgets/logo.dart
+++ lib/widgets/logo.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class Logo extends StatelessWidget {
-  const Logo({Key? key}) : super(key: key);
+  final double? width;
+
+  const Logo({Key? key, this.width = 100}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
-      'assets/images/flutter-logo.svg',
-      width: 100,
+      'assets/images/logo.svg',
+      width: width,
     );
   }
 }
